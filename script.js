e12c226395c6464e739192880a3c9d4b9f53a200
kmerediith <kymered24@gmail.com>
2023-10-11 17:24:26 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const myBike = {
+    tire: true, 
+    spokes: true, 
+    inflated: false, 
+    handlebar: true, 
+    condition: "fair"
+};
+console.log(Object.keys(myBike));
+console.log(Object.values(myBike));
